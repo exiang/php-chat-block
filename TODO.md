4196ccdccac5e74a85d1f461baa56ff2350e260a
cth-inni <charlie.tang@innity.com>
2021-08-31 18:01:58 +0800

    Fixing, add features
    - [x] rename css file to chatblock.css
    - [x] adding chatblock.js
    - [x] H1-H6 tag
    - [x] p tag
    - [x] Fix narrator display
    - [x] Conversation index (default as #)
    - [x] minimal format (now can run without the characters settings)
    - [x] linebreak
    - [x] Directly wrote as paragraph will be ignore. (php error free)
    - [x] adding 'rawscript#' for display the raw data before parse.
    - [x] setColon() fixed
    - [x] setNarrator() fixed
    - [x] Colon array (default as #)
    - [x] Narrator array (default as narrator)
    - [x] stater template
    - [x] dev tutorial （with sample）

---
+++ TODO.md
@@ -0,0 +1,30 @@
+# version 1 roadmap
+## Aug 2021
+- [x] rename css file to chatblock.css
+- [x] adding chatblock.js
+- [x] H1-H6 tag
+- [x] p tag
+- [x] Fix narrator display
+- [x] Conversation index (default as #)
+- [x] minimal format (now can run without the characters settings)
+- [x] linebreak
+- [x] Directly wrote as paragraph will be ignore. (php error free)
+- [x] adding 'rawscript#' for display the raw data before parse.
+- [x] setColon() fixed
+- [x] setNarrator() fixed
+- [x] Colon array (default as #)
+- [x] Narrator array (default as narrator)
+- [x] stater template
+- [x] dev tutorial （with sample）
+
+# version 2 roadmap
+- [] global setting before render
+- [] default head image for minor casts (assign a random background color)
+- [] JS editor (shortcut & preview)
+- [] export format (txt,json,csv,custom)
+
+
+
+# refer
+对话里面，如果有这种卡片效果的插图，会打扰阅读体验吗？
+https://codepen.io/nicolaspavlotsky/pen/wqGgLO?editors=1100
--- src/ChatBlock.php
+++ src/ChatBlock.php
@@ -3,65 +3,101 @@ namespace TangHoong\ChatBlock;
 
 class ChatBlock
 {
+    public $rawData;
     public $colonList;
     public $narratorList;
+    public $rolesList;
     public $roles;
     public $lines;
     public $dialogue;
+    public $output;
+    public $SettingBreakPoint;
+    public $SettingWhitelistTag;
     function __construct()
     {
         // 
+        $this->rawData = '';
+        $this->rolesList = [];
+        $this->SettingBreakPoint = "---";
+        $this->SettingWhitelistTag = [
+            'p','h1','h2','h3','h4','h5','h6','linebreak',
+            'image','imagecard','mp3','background','youtube','decision',
+            'rawscript','codeblock',
+            'narrator',
+        ];
+        $this->colonList = ["#"];
+        $this->narratorList = ["narrator"];
     }
-    public function feed($data='')
+    public function feed($rawData='')
     {
-        // $this->colonList = [':','：'];
+        $this->rawData = $rawData;
         $chat['casts']  = 0;
         $chat['dialog'] = 0;
         $chat['roles']  = [];
         $chat['lines']  = [];
-        $rolesData = strstr($data, "---", true);
-        $linesData = strstr($data, "---");
-        // structure roles
-        $rolesArray = array_values(array_filter(explode(PHP_EOL,$rolesData)));
-        $chat['casts'] = count($rolesArray);
-        foreach($rolesArray as $roleKey => $roleVal)
-        {
-            $tempCast = [];
-            // foreach($this->colonList as $colon)
-            // {
+        $rolesData = strstr($rawData, $this->SettingBreakPoint, true);
+        $linesData = strstr($rawData, $this->SettingBreakPoint);
+        if($rolesData != false)
+        { // structure roles
+            $rolesArray = array_values(array_filter(explode(PHP_EOL,$rolesData)));
+            $chat['casts'] = count($rolesArray);
+            foreach($rolesArray as $roleKey => $roleVal)
+            {
+                $tempCast = [];
                 $tempArray = explode("@",$roleVal);
                 if(isset($tempArray))
                 {
-                    // var_dump($tempArray);
                     list($name, $img) = $tempArray;
                     $tempCast['name']  = $name;
                     $tempCast['img']   = $img;
                     array_push($chat['roles'],$tempCast);
+                    array_push($this->rolesList,$name);
+                    array_push($this->SettingWhitelistTag,$name);
                 }
-            // }
+            }
         }
-        // structure lines
-        $linesArray = array_values(array_filter(explode(PHP_EOL,$linesData)));
-        foreach($linesArray as $lineKey => $lineVal)
-        {
-            if($lineVal != '---')
+        if($linesData != false)
+        { // structure lines // reading image header settings, render with name + image
+            $linesArray = array_values(array_filter(explode(PHP_EOL,$linesData)));
+            foreach($linesArray as $lineKey => $lineVal)
             {
-                $chat['dialog'] = $chat['dialog'] + 1;
-                $tempLine = [];
-                // foreach($this->colonList as $colon)
-                // {
-                    // var_dump($colon);
-                    // $tempArray = explode($colon,$lineVal);
-                    $tempArray = explode(":",$lineVal);
-                    if(isset($tempArray))
+                if($lineVal != $this->SettingBreakPoint)
+                {
+                    $chat['dialog'] = $chat['dialog'] + 1;
+                    $tempLine = [];
+                    foreach($this->colonList as $colon)
                     {
-                        // var_dump($tempArray);
-                        list($name, $sentence) = $tempArray;
-                        $tempLine['name']  = $name;
-                        $tempLine['sentence']   = $sentence;
-                        array_push($chat['lines'],$tempLine);
+                        $tempArray = explode($colon,$lineVal);
+                        if(isset($tempArray) && count($tempArray) > 1 && in_array($tempArray[0],$this->SettingWhitelistTag))
+                        { // whitelisted
+                            list($name, $sentence) = $tempArray;
+                            $tempLine['name']  = $name;
+                            $tempLine['sentence']   = $sentence;
+                            array_push($chat['lines'],$tempLine);
+                        }
                     }
-                // }
+                }
+            }
+        }else{ // structure lines // Without those image header settings, allow them to render by name only
+            $linesArray = array_values(array_filter(explode(PHP_EOL,$rawData)));
+            foreach($linesArray as $lineKey => $lineVal)
+            {
+                if($lineVal != $this->SettingBreakPoint)
+                {
+                    $chat['dialog'] = $chat['dialog'] + 1;
+                    $tempLine = [];
+                    foreach($this->colonList as $colon)
+                    {
+                        $tempArray = explode($colon,$lineVal);
+                        if(isset($tempArray) && count($tempArray) > 1)
+                        { // whitelisted
+                            list($name, $sentence) = $tempArray;
+                            $tempLine['name']  = $name;
+                            $tempLine['sentence']   = $sentence;
+                            array_push($chat['lines'],$tempLine);
+                        }
+                    }
+                }
             }
         }
         $this->dialogue = $chat;
@@ -79,7 +115,7 @@ class ChatBlock
         $this->colonList = $colonArray;
     }
     /**
-     * Set Colon
+     * Set Narrator
      */
     public function setNarrator($narratorArray = []){
         $this->narratorList = $narratorArray;
@@ -87,50 +123,86 @@ class ChatBlock
     /**
      * Using default html rendered chat blocks
      */
+    public function output(){
+        return $this->output;
+    }
     public function render(){
         $tempHtml = '<div class="chatblock">';
         foreach($this->dialogue['lines'] as $dialogue)
         {
             switch($dialogue['name'])
             {
-                case 'Narrator': 
-                    $tempHtml .= $this->role_narrator($dialogue);
+                case 'h1': 
+                case 'h2': 
+                case 'h3': 
+                case 'h4': 
+                case 'h5': 
+                case 'h6': 
+                    $tempHtml .= $this->render_heading($dialogue);
                 break;
-                case 'Flipcard': 
-                    $tempHtml .= $this->render_flipcard_holder($dialogue);
+                case 'p': 
+                    $tempHtml .= $this->render_text($dialogue);
                 break;
-                case 'Image': 
+                case 'rawscript': 
+                    $tempHtml .= $this->render_rawdata($dialogue,$this->rawData);
+                break;
+                case 'codeblock': 
+                    $tempHtml .= $this->render_codeblock($dialogue);
+                break;
+                case 'linebreak': 
+                    $tempHtml .= '<hr/>';
+                break;
+                case 'image': 
                     $tempHtml .= $this->render_image_holder($dialogue);
                 break;
-                case 'MP3': 
-                case 'Background': 
+                case 'imagecard': 
+                    $tempHtml .= $this->render_imagecard_holder($dialogue);
+                break;
+                case 'mp3': 
+                case 'background': 
                     $tempHtml .= $this->render_sound_holder($dialogue);
                 break;
-                case 'Youtube': 
+                case 'youtube': 
                     $tempHtml .= $this->render_video_holder($dialogue);
                 break;
-                case 'Decision': 
+                case 'decision': 
                     $tempHtml .= $this->render_decisions_holder($dialogue);
                 break;
-                case $this->dialogue['roles'][0]['name']: 
-                    $tempHtml .= $this->role_rightSide($dialogue);
+                case 'narrator': 
+                    $tempHtml .= $this->role_narrator($dialogue);
                 break;
                 default: 
-                    $tempHtml .= $this->role_leftSide($dialogue);
+                    if(in_array($dialogue['name'],$this->narratorList))
+                    { // Custom narrator
+                        $tempHtml .= $this->role_narrator($dialogue);
+                    }else{
+                        if( isset($this->rolesList[0]) && $this->rolesList[0] == $dialogue['name'])
+                        { // maincast
+                            $tempHtml .= $this->role_rightSide($dialogue);
+                        }else{
+                            $tempHtml .= $this->role_leftSide($dialogue);
+                        }
+                    }
                 break;
             }
         }
         $tempHtml .= '</div>';
-        return $tempHtml;
+        $this->output = $tempHtml;
+    }
+    public static function renderJs()
+    {
+        ob_start();
+        require 'chatblock.js';
+        return ob_get_clean();
     }
     public static function renderCss()
     {
         ob_start();
-        require 'imessage.css';
+        require 'chatblock.css';
         return ob_get_clean();
     }
     // Multimedia
-    private function render_flipcard_holder($dialogue)
+    private function render_imagecard_holder($dialogue)
     {
         $link = $this->fn_valid_link($dialogue['sentence']);
         $url_components = parse_url($link);
@@ -141,7 +213,7 @@ class ChatBlock
         $tempHtml  .= '<div class="flip-card">';
         $tempHtml  .= '<div class="flip-card-inner">';
         $tempHtml  .= '<div class="flip-card-front">';
-        $tempHtml  .= '<img src="'.$link.'" alt="Flipcard" style="width:100%;height:100%;">';
+        $tempHtml  .= '<img src="'.$link.'" alt="imagecard" style="width:100%;height:100%;">';
         $tempHtml  .= '</div>';
         $tempHtml  .= '<div class="flip-card-back">';
         if($title)
@@ -155,6 +227,34 @@ class ChatBlock
         $tempHtml  .= '</div>';
         $tempHtml  .= '</div>';
         $tempHtml  .= '</div>';
+        return $tempHtml;
+    }
+    private function render_rawdata($dialogue, $rawData)
+    {
+        // $your_array = explode("\n", $rawData);
+        // $arr = explode("\n", $your_array);
+        $tempHtml  = '<pre><code>'.($rawData).'</code></pre>';
+        return $tempHtml;
+    }
+    private function render_codeblock($dialogue)
+    {
+        $sentence  = $this->fn_filter($dialogue['sentence']);
+        // $sentence  = ($dialogue['sentence']);
+        $tempHtml  = '<pre><code>'.$sentence.'</code></pre>';
+        return $tempHtml;
+    }
+    private function render_text($dialogue)
+    {
+        $sentence  = $this->fn_filter($dialogue['sentence']);
+        $tempHtml  = '<p class="comment">'.$sentence.'</p>';
+        return $tempHtml;
+    }
+    private function render_heading($dialogue)
+    {
+        $link = $this->fn_valid_link($dialogue['sentence']);
+        $tempHtml   = '<'.strtolower($dialogue['name']).'>';
+        $tempHtml  .= $dialogue['sentence'];
+        $tempHtml  .= '</'.strtolower($dialogue['name']).'>';
         return $tempHtml;
     }
     private function render_image_holder($dialogue)
@@ -175,7 +275,7 @@ class ChatBlock
         $tempHtml  .= '</audio>';
         if($dialogue['name'] == 'Background')
         {
-        $tempHtml  .= '<div class="text-muted text-bold text-center">Background Music</div>';
+        $tempHtml  .= '<div class="text-muted text-bold text-center">背景循环音乐</div>';
         }
         $tempHtml  .= '</div>';
         return $tempHtml;
@@ -190,8 +290,9 @@ class ChatBlock
     }
     private function render_decisions_holder($dialogue)
     {
-        $optionList = explode(',',$dialogue['sentence']);
-        $tempHtml   = '<p class="text-center comment">Your decisions are</p>';
+        $paramItems = explode('=',$dialogue['sentence']);
+        $optionList = explode(',',$paramItems[1]);
+        $tempHtml   = '<p class="text-center comment">'.$paramItems[0].'</p>';
         $tempHtml  .= '<div class="container-decision">';
         foreach($optionList as $option)
         {
@@ -219,7 +320,9 @@ class ChatBlock
     private function role_narrator($dialogue)
     {
         $sentence  = $this->fn_filter($dialogue['sentence']);
-        $tempHtml  = '<p class="comment">'.$sentence.'</p>';
+        $tempHtml  = '<div class="imessage">';
+        $tempHtml .= '<p class="narrator">'.$sentence.'</p>';
+        $tempHtml .= '</div>';
         return $tempHtml;
     }
     private function role_leftSide($dialogue)
